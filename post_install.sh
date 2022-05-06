58cf812719817604b6031fe128f2edd7b9d00a1f
sebostien <info@nielsen.run>
2022-05-06 16:58:45 +0200

    Some fonts

--- post_install.sh
+++ post_install.sh
@@ -253,6 +253,9 @@ if ask "Install fonts?"; then
     
     sudo dnf install unzip -y
 
+    # Microsoft fonts
+    sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
+    sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
     
     # Roboto
     sudo dnf install google-roboto-fonts

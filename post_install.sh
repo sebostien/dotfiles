1b1bbb04443c80876507e3a108c2b2eac946545b
sebostien <info@nielsen.run>
2022-05-29 01:33:09 +0200

    Add programs

--- post_install.sh
+++ post_install.sh
@@ -145,7 +145,7 @@ sudo dnf install -y network-manager-applet blueman pulseaudio-utils
 sudo dnf install neofetch flameshot fzf bat tldr \
                  httpie alacritty exa rofi nitrogen \
                  nautilus dunst neovim playerctl \
-                 pulseaduio vlc ffmpeg qalculate-gtk -y
+                 pulseaduio vlc ffmpeg qalculate-gtk btop -y
 
 sudo dnf install steam -y
 sudo dnf install trayer -y

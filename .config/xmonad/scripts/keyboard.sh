c8876b9e91390c8f699b3ca62483f509d31e175c
sebostien <sebostien@proton.me>
2023-04-12 11:27:45 +0200

    Fix page-keys

--- .config/xmonad/scripts/keyboard.sh
+++ .config/xmonad/scripts/keyboard.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
-
 current=$(localectl | grep X11 | awk '{print $3}')
 
 if [ "$current" != se ]; then
-  # Disable page-up/down on laptop
-  if [ -f "/home/sn/.is_desktop" ]; then
-    xmodmap -e 'keycode 117='
-    xmodmap -e 'keycode 112='
-  fi
   localectl set-x11-keymap se
   localectl set-keymap se
   setxkbmap se -option caps:swapescape
   eww update locale="se"
-else
   # Disable page-up/down on laptop
-  if [ -f "/home/sn/.is_desktop" ]; then
+  if [ -f "/home/sn/.is_desktop" ]; then 
+    echo;
+  else
     xmodmap -e 'keycode 117='
     xmodmap -e 'keycode 112='
-  fi
+  fi 
+else
   localectl set-x11-keymap us
   localectl set-keymap us
   setxkbmap us -option caps:swapescape
   eww update locale="us"
+  # Disable page-up/down on laptop
+  if [ -f "/home/sn/.is_desktop" ]; then 
+    echo;
+  else
+    xmodmap -e 'keycode 117='
+    xmodmap -e 'keycode 112='
+  fi
 fi

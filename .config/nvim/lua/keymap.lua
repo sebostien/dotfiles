be944743400e0b9cb98bb0d3e990d6045b364c50
sebostien <sebostien@proton.me>
2023-05-19 17:02:23 +0200

    Keymap to open pdf with zathura

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -7,8 +7,8 @@ local function opt_sn_desc(desc)
   return { silent = true, noremap = true, desc = desc }
 end
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+vim.g.mapleader = " " -- Commands involving current buffer
+vim.g.maplocalleader = "," -- Commands beyond buffer
 
 -- Jump between windows
 keymap("n", "<C-k>", "<C-w>k", opt_sn)
@@ -26,8 +26,8 @@ keymap("n", "<C-Right>", "<CMD>vertical resize +5<CR>", opt_sn)
 keymap("n", "<leader>h", "<CMD>nohlsearch<CR>", opt_sn)
 
 -- Lsp Stuff
-keymap("n", "<leader>lr", "<CMD>LspRestart<CR><CMD>e<CR>", opt_sn_desc("Restart lsp servers"))
-keymap("n", "<leader>ls", "<CMD>LspStop<CR><CMD>lua vim.diagnostic.reset()<CR>", opt_sn_desc("Stop lsp servers"))
+keymap("n", "<localleader>lr", "<CMD>LspRestart<CR><CMD>e<CR>", opt_sn_desc("Restart lsp servers"))
+keymap("n", "<localleader>ls", "<CMD>LspStop<CR><CMD>lua vim.diagnostic.reset()<CR>", opt_sn_desc("Stop lsp servers"))
 keymap("n", "<localleader>ll", "<CMD>LspInfo<CR>", opt_sn)
 keymap("n", "<localleader>lm", "<CMD>Mason<CR>", opt_sn)
 keymap("n", "<localleader>ln", "<CMD>NullLsInfo<CR>", opt_sn)
@@ -38,15 +38,11 @@ keymap("n", "<localleader>ms", "<CMD>lua require('harpoon.ui').toggle_quick_menu
 keymap("n", "<localleader>mf", "<CMD>Telescope harpoon marks<CR>", opt_sn)
 
 -- Current buffer toggles
-keymap("n", "<leader>tc", "<CMD>CccHighlighterToggle <CR>", opt_sn)
-keymap("n", "<leader>ti", "<CMD>IlluminateToggle<CR>", opt_sn)
+keymap("n", "<localleader>tc", "<CMD>CccHighlighterToggle <CR>", opt_sn)
+keymap("n", "<localleader>ti", "<CMD>IlluminateToggle<CR>", opt_sn)
 
 -- UndoTree
 keymap("n", "<localleader>u", "<CMD>UndotreeToggle<CR>", opt_sn)
-
--- Overseer
-keymap("n", "<localleader>ot", "<CMD>OverseerToggle<CR>", opt_sn)
-keymap("n", "<localleader>or", "<CMD>OverseerRun<CR>", opt_sn)
 
 -- Packer
 keymap("n", "<localleader>ps", "<CMD>PackerSync<CR>", opt_sn)
@@ -64,6 +60,11 @@ keymap("n", "<localleader>fb", ":Telescope buffers<CR>", opt_sn)
 keymap("n", "<localleader>fh", ":Telescope command_history<CR>", opt_sn)
 keymap("n", "<localleader>fcd", util.telescope_cd, opt_sn_desc("Change directory"))
 
+-- Open files
+keymap("n", "<localleader>op", function()
+  util.telescope_files({"%.pdf"}, "zathura")
+end, opt_sn_desc("Open file in zathura"))
+
 -- Help
 keymap("n", "<localleader>hh", ":Telescope help_tags<CR>", opt_sn)
 keymap("n", "<localleader>hm", ":Telescope man_pages<CR>", opt_sn)
@@ -73,9 +74,13 @@ keymap("n", "<localleader>hk", ":Telescope keymaps<CR>", opt_sn)
 keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opt_sn)
 keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opt_sn)
 
--------------------
--- Visual ---------
--------------------
+-- Search replace word under cursor
+keymap(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  opt_sn_desc("Substitue word under cursor")
+)
 
 -- Move lines
 keymap("x", "<A-j>", ":m '>+1<CR>gv-gv", opt_sn)
@@ -104,8 +109,3 @@ keymap("n", "n", "nzz", opt_sn)
 keymap("n", "N", "Nzz", opt_sn)
 
 keymap("n", "Q", "<nop>", opt_sn)
-
-local tmux = require("tmux-util")
-keymap("n", "<localleader>tn", tmux.new_session, opt_sn_desc("New tmux session"))
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])

6eec50706f59268c21d4c17ce2ab10b77309ec5a
sebostien <sebostien@proton.me>
2023-08-28 21:14:56 +0200

    Fix bug with x11 locale order

--- .config/xmonad/scripts/keyboard.sh
+++ .config/xmonad/scripts/keyboard.sh
@@ -2,10 +2,10 @@
 
 
 current=$(localectl | grep X11 | awk '{print $3}')
-
+echo $current
 if [ "$current" != se ]; then
-    localectl set-x11-keymap se
     localectl set-keymap se
+    localectl set-x11-keymap se
     setxkbmap se -option caps:swapescape
     eww update locale="se"
     # Disable page-up/down on laptop
@@ -14,8 +14,8 @@ if [ "$current" != se ]; then
         xmodmap -e 'keycode 112='
     fi
 else
-    localectl set-x11-keymap us
     localectl set-keymap us
+    localectl set-x11-keymap us
     setxkbmap us -option caps:swapescape
     eww update locale="us"
     # Disable page-up/down on laptop
